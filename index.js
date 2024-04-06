4fe4f4fba11819bbde1673b21c797fcd1a243fec
divyeshjethva <162811798+divyeshjethva@users.noreply.github.com>
2024-04-06 18:05:19 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,76 @@
+let a='';
+
+function my1(){
+    a= a+1;
+    document.querySelector('.out').innerHTML=a;
+}
+function my2(){
+    a= a+2;
+    document.querySelector('.out').innerHTML=a;
+}
+function my3(){
+    a= a+3;
+    document.querySelector('.out').innerHTML=a;
+}
+function my4(){
+    a= a+4;
+    document.querySelector('.out').innerHTML=a;
+}
+function my5(){
+    a= a+5;
+    document.querySelector('.out').innerHTML=a;
+}
+function my6(){
+    a= a+6;
+    document.querySelector('.out').innerHTML=a;
+}
+function my7(){
+    a= a+7;
+    document.querySelector('.out').innerHTML=a;
+}
+function my8(){
+    a= a+8;
+    document.querySelector('.out').innerHTML=a;
+}
+function my9(){
+    a= a+9;
+    document.querySelector('.out').innerHTML=a;
+}
+function my0(){
+    a= a+0;
+    document.querySelector('.out').innerHTML=a;
+}
+
+
+function myac(){
+    a = '';
+    document.querySelector('.out').innerHTML = a;
+}
+function mye(){
+    let result;
+    result = eval(a); 
+    document.querySelector('.out').innerHTML = result;
+}
+
+
+
+function myd(){
+    a= a+'/';
+    document.querySelector('.out').innerHTML=a;
+}
+function mym(){
+    a= a+'*';
+    document.querySelector('.out').innerHTML=a;
+}
+function mys(){
+    a= a+'-';
+    document.querySelector('.out').innerHTML=a;
+}
+function mya(){
+    a= a+'+';
+    document.querySelector('.out').innerHTML=a;
+}
+function mydot(){
+    a= a+'.';
+    document.querySelector('.out').innerHTML=a;
+}
